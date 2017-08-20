cd2d62cbd94d2908783076b702f48d23ff3148f4
Khai Hoang <khaihoangdev@gmail.com>
2017-08-20 08:39:51 +0700

    ok

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var data = require("./data/postData");
-
+var path = require("path");
 var app = express();
-app.set("data", data);
 
+app.set("data", data);
+console.log(path.join(__dirname, "public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(require("./routes/index"));
 app.use(require("./routes/posts"));
 
